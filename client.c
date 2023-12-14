ef4496116c6033358b08bb8c06b969bc4fe6a5c9
Khaled Korfali <46685494+KhaledKorfali2@users.noreply.github.com>
2023-12-14 11:04:41 -0800

    Add files via upload

--- client.c
+++ client.c
@@ -1,74 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 2048
-#define RESPONSE_SIZE 4096
+#define LENGTH 2048
 
-int main() {
-    int client_socket;
-    struct sockaddr_in server_address;
-    char buffer[BUFFER_SIZE];
-    char response[RESPONSE_SIZE];
+// Global variables
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[32];
 
-    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+void str_overwrite_stdout() {
+    printf("%s", "> ");
+    fflush(stdout);
+}
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(PORT);
-
-    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
-        perror("Connection failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Enter your username: ");
-    fgets(buffer, sizeof(buffer), stdin);
-    buffer[strcspn(buffer, "\n")] = '\0';
-
-    send(client_socket, buffer, strlen(buffer), 0);
-
-    ssize_t received = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (received <= 0) {
-        perror("Error receiving welcome message");
-        close(client_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server says: %.*s\n", (int)received, buffer);
-
-    // Start chat loop
-    while (1) {
-        // Prompt user to enter text
-        printf("Enter your message (type 'quit' to exit): ");
-        fgets(buffer, sizeof(buffer), stdin);
-        buffer[strcspn(buffer, "\n")] = '\0'; // Remove trailing newline
-
-        // Send user's message to the server
-        send(client_socket, buffer, strlen(buffer), 0);
-
-        // Check if the user wants to quit
-        if (strcmp(buffer, "quit") == 0) {
+void str_trim_lf(char *arr, int length) {
+    int i;
+    for (i = 0; i < length; i++) { // trim \n
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
             break;
         }
+    }
+}
 
-        // Receive and print the server's response
-        received = recv(client_socket, response, sizeof(response), 0);
-        if (received <= 0) {
-            perror("Error receiving server response");
-            break; // Server disconnected
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
+}
+
+
+void recv_msg_handler() {
+    char message[LENGTH] = {};
+    while (1) {
+        int receive = recv(sockfd, message, LENGTH, 0);
+        if (receive > 0) {
+            printf("%s\n", message);
+            str_overwrite_stdout();
+        } else if (receive == 0) {
+            break;
+        } else {
+            // -1
+        }
+        memset(message, 0, sizeof(message));
+    }
+}
+
+void send_msg_handler() {
+    char message[LENGTH] = {};
+    while (1) {
+        str_overwrite_stdout();
+        fgets(message, LENGTH, stdin);
+        str_trim_lf(message, LENGTH);
+
+        if (strcmp(message, "exit") == 0) {
+            break;
+        } else {
+            send(sockfd, message, strlen(message), 0);
         }
 
-        printf("Server says: %.*s\n", (int)received, response);
+        bzero(message, LENGTH);
+    }
+    catch_ctrl_c_and_exit(2);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    close(client_socket);
-    return 0;
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
+
+    signal(SIGINT, catch_ctrl_c_and_exit);
+
+    printf("Please enter your name: ");
+    fgets(name, 32, stdin);
+    str_trim_lf(name, strlen(name));
+
+    if (strlen(name) > 32 || strlen(name) < 2) {
+        printf("Name must be less than 30 and more than 2 characters.\n");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in server_addr;
+
+    /* Socket settings */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
+
+    // Connect to Server
+    int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (err == -1) {
+        printf("ERROR: connect\n");
+        return EXIT_FAILURE;
+    }
+
+    // Send name
+    send(sockfd, name, 32, 0);
+
+    printf("=== WELCOME TO THE CHATROOM ===\n");
+
+    pthread_t send_msg_thread;
+    if (pthread_create(&send_msg_thread, NULL, (void *)send_msg_handler, NULL) != 0) {
+        printf("ERROR: pthread\n");
+        return EXIT_FAILURE;
+    }
+
+    pthread_t recv_msg_thread;
+    if (pthread_create(&recv_msg_thread, NULL, (void *)recv_msg_handler, NULL) != 0) {
+        printf("ERROR: pthread\n");
+        return EXIT_FAILURE;
+    }
+
+    while (1) {
+        if (flag) {
+            printf("\nBye\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+
+    return EXIT_SUCCESS;
 }
 
